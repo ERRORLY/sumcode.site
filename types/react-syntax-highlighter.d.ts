1a26541af9efc1d0045adfdd5d64c7fce24d3090
CYCNO <daniyalchunna2@gmail.com>
2025-08-22 16:10:01 +0530

    fixed prism type error

---
+++ types/react-syntax-highlighter.d.ts
@@ -0,0 +1,7 @@
+declare module "react-syntax-highlighter" {
+  import * as React from "react";
+  export const Prism: any;
+  export const Light: any;
+  export const Dark: any;
+}
+declare module "react-syntax-highlighter/dist/esm/styles/prism";
